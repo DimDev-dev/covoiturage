158912862674c484187439cd2ed8eefe1f8231d5
DimDev <vandevelde.dimitri@hotmail.com>
2023-08-11 00:42:49 +0200

    ajout

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,19 +1,46 @@
-import Link from 'next/link'
-import React from 'react'
-import "../assset/form.css"
+"use client";
+import Link from "next/link";
+import React from "react";
+import "../assset/form.css";
+import { useForm } from "react-hook-form";
 
 const login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data.email);
+
   return (
     <>
-      <form className='formulaire'>
+      <form className="formulaire" onSubmit={handleSubmit(onSubmit)}>
         <p>Connexion</p>
-        <input placeholder='email' type="email" />
-        <input placeholder='mot de passe' type="password" />
+        <input
+          placeholder="email"
+          type="email"
+          {...register("email", {
+            required: "Ce champ est requis",
+          })}
+        />
+        {errors.email && errors.email.message}
+
+        <input
+          placeholder="mot de passe"
+          type="password"
+          {...register("password", {
+            required: "Ce champ est requis",
+          })}
+        />
+        {errors.password && errors.password.message}
+
         <input type="submit" value="Connexion" />
         <Link href="/signup">Vous n'avez pas encore de compte ?</Link>
       </form>
     </>
-  )
-}
+  );
+};
 
-export default login
+export default login;
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,15 +1,51 @@
+"use client";
 import Link from "next/link";
 import React from "react";
-import "../assset/form.css"
+import "../assset/form.css";
+import { useForm } from "react-hook-form";
 
 const signup = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data.email + data.password + data.passwordConfirm);
   return (
     <>
-      <form className="formulaire">
+      <form className="formulaire" onSubmit={handleSubmit(onSubmit)}>
         <p>Inscription</p>
-        <input placeholder="email" type="email" id="email" />
-        <input placeholder="mot de passe" type="password" id="password" />
-        <input placeholder="confirmer le mot de passe" type="password" id="password-confirm" />
+        <input
+          placeholder="email"
+          type="email"
+          id="email"
+          {...register("email", {
+            required: "Ce champ est requis!!!",
+          })}
+        />
+        {errors.email && errors.email.message}
+
+        <input
+          placeholder="mot de passe"
+          type="password"
+          id="password"
+          {...register("password", {
+            required: "Ce champ est requis",
+          })}
+        />
+        {errors.password && errors.password.message}
+
+        <input
+          placeholder="confirmer le mot de passe"
+          type="password"
+          id="passwordConfirm"
+          {...register("passwordConfirm", {
+            required: "Ce champ est requis",
+          })}
+        />
+        {errors.passwordConfirm && errors.passwordConfirm.message}
         <input type="submit" value="Inscription" />
         <Link href="/login">Vous avez deja in compte ?</Link>
       </form>
